13cba9fa3c5c61433787af21e44190a2b61bd3a2
Jochen Leinberger <leinberger@explorative-environments.net>
2015-05-17 23:39:13 +0200

    details

---
+++ parklot.js
@@ -0,0 +1,5 @@
+<div class="coverBackground" style="background-image: url('{{pageData.imagePath}}')">
+  <h1>{{pageData.title}}.</h1>
+  <a class="bold" href="#"> overview </a>
+  <a class="bold" href="#/{{pageData.linkToID}}">next</a>
+</div>
